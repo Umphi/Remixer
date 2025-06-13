c09fe410b4d36dc3bfe655fdcecf182603eecab7
Umphi <iforza1@yandex.ru>
2025-06-13 07:14:47 +0700

    Resources loader docstrings added

--- core/resource_loader.py
+++ core/resource_loader.py
@@ -1,14 +1,21 @@
+"""
+Loads resources from internal storage.
+Used in pre-built version of Remixer.
+"""
 import os
 import sys
 
 
-class Loader:
-
+class Loader: # pylint: disable=too-few-public-methods # Aknowledged
+    """
+    Loads resources from internal storage.
+    Used in pre-built version of Remixer.
+    """
     @staticmethod
     def resource_path(relative_path):
         """
         Loads resources from internal storage.
-        Used in pre-built version of Remixer
+        Used in pre-built version of Remixer.
         Parameters:
             relative_path (str): Path to resource.
         """
